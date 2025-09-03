0dbee90027daf6f0b20cd3bd28d3262aebc23f00
mahln908 <mazinholn18@gmail.com>
2025-09-03 01:40:18 -0300

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,34 +1,3 @@
-// sw.js - Service Worker para PWA
-const CACHE_NAME = 'sabor-de-casa-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/receita.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
-
-// Instalação do Service Worker
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Interceptar requisições
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Retorna o arquivo do cache se encontrado
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+// sw.js simplificado
+self.addEventListener('install', e => e.waitUntil(caches.open('pwa-cache')));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
